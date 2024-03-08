d1665d231ca639fc6e7dfeda5c109952278c6e93
mayzalunaz <mayzalunarestanti@gmail.com>
2024-03-08 22:25:49 +0700

    upload 1

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+namespace PercobaanApi1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("api/person")]
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext();
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+
+        [HttpGet("api/person/id")]
+        public ActionResult <Person> GetPersonById(int id) 
+        {
+            PersonContext context = new PersonContext();
+            var person = context.GetPersonById(id);
+            if (person != null) 
+            {
+                return Ok(person);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
